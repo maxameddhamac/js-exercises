cafaa745f440f024875b1f09bba1c0899b56a6e2
Mohamed Ibrahim <zhaaficimaxamed12@gmail.com>
2026-09-06 19:58:57 +0300

    waa exercise 4

---
+++ exercise4/app4.js
@@ -0,0 +1,17 @@
+function add(a, b) {
+  console.log(a + b);
+}
+
+console.log(2 + 3);
+console.log(4 + 3);
+console.log(5 + 3);
+
+// suaashi labaad waa tan⬇️
+
+const add1 = function (a, b) {
+  console.log(a + b);
+};
+
+console.log(2 + 3);
+console.log(4 + 3);
+console.log(5 + 3);
