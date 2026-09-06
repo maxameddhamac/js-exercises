914c819a82302a90837d39b868cf2185fd1b06e1
Mohamed Ibrahim <zhaaficimaxamed12@gmail.com>
2026-09-06 18:36:46 +0300

    waakn

---
+++ exercise2/app2.js
@@ -0,0 +1,11 @@
+let a = 10;
+b = 5;
+
+console.log(a == b);
+console.log(a != b);
+console.log(a === b);
+console.log(a !== b);
+console.log(a > b);
+console.log(a < b);
+console.log(a >= b);
+console.log(a <= b);
