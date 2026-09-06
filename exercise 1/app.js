3d2c2c4a1bd8b83421965c66ea91c527070dfa71
Mohamed Ibrahim <zhaaficimaxamed12@gmail.com>
2026-09-06 17:51:35 +0300

    waakan exercise kii

---
+++ exercise 1/app.js
@@ -0,0 +1,16 @@
+let x = 7;
+y = 3;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+
+console.log(x++);
+console.log(x--);
+
+console.log(++y);
+console.log(--y);
+console.log(x);
+console.log(y);
