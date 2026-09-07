1a48954ebff5b2e79445353426a1b36d86b3a3cf
Mohamed Ibrahim <zhaaficimaxamed12@gmail.com>
2026-09-07 21:12:14 +0300

    exercise10

---
+++ exercise10/app10.js
@@ -0,0 +1,12 @@
+let libary = [
+  {
+    name: "ha weynaanin aabe",
+    author: "Aadan sahan",
+    year: 2020,
+  },
+
+  { name: "Hal Ka Haleel", author: " Maxamed Baashe X. Xasan ", year: 2018 },
+];
+
+console.log(libary[0].name);
+console.log(libary[1].author);
