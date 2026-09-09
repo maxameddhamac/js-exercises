b686894a0645d8381fe84a88003290e0580c3045
Mohamed Ibrahim <zhaaficimaxamed12@gmail.com>
2026-09-09 11:34:34 +0300

    wa exercise 17

---
+++ Exercise17/app17.js
@@ -0,0 +1,10 @@
+let temp = 30;
+if (temp < 0) {
+  console.log("very cold");
+} else if (temp <= 15) {
+  console.log("cold");
+} else if (temp <= 25) {
+  console.log("warm");
+} else {
+  console.log("hot");
+}
